6fab08643cc7b3b149de11e530fd1dff32d7006a
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-07-19 13:38:41 +0700

    update connect mongodb

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from 'cors';
 import morgan from "morgan"
 import mongoose from "mongoose"
+import { MongoClient } from "mongodb";
 
 
 const app = express();
@@ -11,9 +12,11 @@ app.use(cors());
 app.use(morgan("tiny"))
 app.use(express.json());
 
+const url = "mongodb+srv://nodejsgroup8:nodejsgroup8@cluster0.btydm.mongodb.net/NodeJS?retryWrites=true&w=majority"
+const mongo = new MongoClient(url, { useNewUrlParser: true });
 
 // connect db
-mongoose.connect("mongodb://localhost:27017/we16310")
+mongo.connect("")
     .then(() => {
         console.log("Kết nối DB thành công");
     })
