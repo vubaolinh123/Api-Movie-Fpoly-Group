1e300cacf408d6a1a41166115f1a1fbf67fffb7c
vintph16172 <vintph16172@fpt.edu.vn>
2022-08-02 12:25:08 +0700

    feature: AdminUser

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { changpassword, signin, signinwithnextauth, signup } from "../controllers/auth";
+import { changpassword, detailUser, editUser, listUser, signin, signinwithnextauth, signup } from "../controllers/auth";
+
 
 const router = Router()
 
@@ -7,5 +8,11 @@ router.post("/signin", signin)
 router.post("/signup", signup)
 router.post("/signinwithnextauth", signinwithnextauth)
 router.post("/changepass", changpassword)
+
+//----------------AUTH---------------------
+
+router.get("/users", listUser )
+router.get("/users/:id", detailUser )
+router.put("/users/:id", editUser )
 
 export default router
